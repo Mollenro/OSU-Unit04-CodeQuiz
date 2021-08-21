8c3c8a1fcce79c8bd006b090b85ca75665e22a00
mollenro <mollenro@gmail.com>
2021-08-21 17:03:30 -0400

    Adding to the JavaScript and adding comments for future reference

---
+++ assets/script.js
@@ -0,0 +1,83 @@
+//An array of objects! ex questions[0].title probably returns the first title
+let questions = [
+      {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+      {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+          "numbers and strings",
+          "other arrays",
+          "booleans",
+          "all of the above"
+        ],
+        answer: "all of the above"
+      },
+      {
+        title:
+          "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+      },
+      {
+        title:
+          "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+      }
+    ];
+
+
+let startBtn = $("#startQuiz");
+let timeEl = $("#timer")
+let questionNum = 0;
+//As question answered correctly qnum++;?
+startBtn.on('click', function(){
+    //code here
+    startTimer();
+})
+
+function startTimer() {
+    let timeLeft = 75;
+
+    let timer = setInterval(function(){
+        timeLeft--;
+        //updates timer text
+        timerEl.text("Timer: " + timeLeft);
+        if (timeLeft === 0){
+            clearInterval(timer);
+        } //else if all ?'s answered clearInt?
+    }, 1000);
+}
+
+function displayQuestions() {
+    let question = $("<h1>");
+    let ans1 = $("<button>");
+    let ans2 = $("<button>");
+    let ans3 = $("<button>");
+    let ans4 = $("<button>");
+
+
+
+
+
+
+
+
+}
+//funct display question and answers from objects
+//funct determine correct answer
+//display next question
+//funct penalty
+//timer
+//once all questions are done displays input box for initials and score updates local storage and 
+//^places data on the highscores.html page
+
+//maybe make a separate .js file for highscores? it will have access to local files
