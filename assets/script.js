87634826a4eda0fc96e1eb41d2a16e48d422b323
mollenro <mollenro@gmail.com>
2021-08-28 13:40:03 -0400

    Adding functionality via JS. Questions appear on screen

--- assets/script.js
+++ assets/script.js
@@ -12,66 +12,96 @@ let questions = [
       },
       {
         title: "Arrays in JavaScript can be used to store ____.",
-        choices: [
-          "numbers and strings",
-          "other arrays",
-          "booleans",
-          "all of the above"
-        ],
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
       },
       {
-        title:
-          "String values must be enclosed within ____ when being assigned to variables.",
+        title: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes"
       },
       {
-        title:
-          "A very useful tool used during development and debugging for printing content to the debugger is:",
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
         answer: "console.log"
       }
     ];
 
-
+let count = 0;
 let startBtn = $("#startQuiz");
-let timeEl = $("#timer")
+let timeEl = $("#timer");
 let questionNum = 0;
-//As question answered correctly qnum++;?
+//As question answered correctly count++;?
+
 startBtn.on('click', function(){
-    //code here
+    count = 0;
+    displayQuestions();
     startTimer();
 })
 
+let timeLeft = 75;
+
 function startTimer() {
-    let timeLeft = 75;
+    
 
     let timer = setInterval(function(){
         timeLeft--;
         //updates timer text
-        timerEl.text("Timer: " + timeLeft);
+        timeEl.text("Time: " + timeLeft);
         if (timeLeft === 0){
             clearInterval(timer);
+
         } //else if all ?'s answered clearInt?
     }, 1000);
 }
 
+let titleEl = $("#title");
+let containerEl = $("#choiceContainer");
+let ans1 = $("<button>");
+let ans2 = $("<button>");
+let ans3 = $("<button>");
+let ans4 = $("<button>");
+
 function displayQuestions() {
-    let question = $("<h1>");
-    let ans1 = $("<button>");
-    let ans2 = $("<button>");
-    let ans3 = $("<button>");
-    let ans4 = $("<button>");
+    //Populating the web page with the questions and titles
+    titleEl.text(questions[count].title);
 
+    containerEl.append(ans1.text(questions[count].choices[0]));
+    containerEl.append(ans2.text(questions[count].choices[1]));
+    containerEl.append(ans3.text(questions[count].choices[2]));
+    containerEl.append(ans4.text(questions[count].choices[3]));
+}
+console.log(questions[count].answer);
 
-
-
-
-
-
+let choice = "";
+function checkAnswer(choice){
+    //dock time if answer is wrong
+    if (choice === questions[count].answer){
+        console.log("correct!");
+        count++;
+        displayQuestions();
+    } else {
+        timeLeft -= 10;
+    }
 
 }
+
+ans1.on('click', function(){
+    choice = ans1.text();
+    checkAnswer(choice);
+})
+ans2.on('click', function(){
+    choice = ans2.text();
+    checkAnswer(choice);
+})
+ans3.on('click', function(){
+    choice = ans3.text();
+    checkAnswer(choice);
+})
+ans4.on('click', function(){
+    choice = ans4.text();
+    checkAnswer(choice);
+})
 //funct display question and answers from objects
 //funct determine correct answer
 //display next question
